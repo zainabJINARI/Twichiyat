4677085831cc7e3bdf29c8a5cbd74e54428d7b5e
HananeOulouchoutam <121303274+HananeOulouchoutam@users.noreply.github.com>
2023-12-27 21:36:50 +0100

    login lougout
    login logout

--- Twichiyat/userP/views.py
+++ Twichiyat/userP/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import CreateUser,UserCreationForm , AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login 
+from django.http import HttpResponse
 
 
 from django.shortcuts import render,redirect
@@ -10,15 +11,13 @@ from .models import Profile
 from . import forms
 
 
-# Create your views here.
 
+# Create your views here.
 def signup_view(request):
     if request.method == 'POST':
         form = CreateUser(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-
-            # Enregistrez d'abord l'utilisateur
             user.save()
 
             # Créez une instance de Profile et liez-la à l'utilisateur
@@ -46,17 +45,26 @@ def signup_view(request):
 
 
 
-def login_view(request):
 
+def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid() :
             user=form.get_user()
             login(request , user)
+<<<<<<< Updated upstream
             profiles = Profile.objects.all()
             for p in profiles :
                 if p.is_vendor == True :
                     return redirect('vendor_dashboard:dashboard')
+=======
+            if user.is_vendor == True :
+                return redirect('vendor_dashboard:dashboard')
+            else :
+                if 'next' in request.POST :
+                    # return  redirect(request.POST.get('next'))
+                    return HttpResponse("Hi")
+>>>>>>> Stashed changes
                 else :
                     if 'next' in request.POST :
                         return  redirect(request.POST.get('next'))
@@ -68,8 +76,15 @@ def login_view(request):
     else:
         form =AuthenticationForm()
         return render(request,'userP/login.html' , { 'form':form })
+<<<<<<< Updated upstream
     
     
 def logout_view(request):
     logout(request)
     return redirect('home')       
+=======
+        
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+>>>>>>> Stashed changes
