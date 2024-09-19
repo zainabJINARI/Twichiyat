384ccc8172098a1986bd34a13ba0ba7d411530bd
Zainab Jinari <111095997+zainabJINARI@users.noreply.github.com>
2024-09-19 18:35:08 +0100

    push

--- Twichiyat/build_files.sh
+++ Twichiyat/build_files.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-
 # Ensure pip is installed and upgrade pip
-python3.9 -m ensurepip --upgrade
+python3.10 -m ensurepip --upgrade
 
-python3.9 -m pip install --upgrade pip setuptools wheel
+python3.10 -m pip install --upgrade pip setuptools wheel
+
+# Install distutils separately
+python3.10 -m pip install distutils
+
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
 # Collect static files
-pip install --disable-pip-version-check --target . --upgrade -r requirements.txt
-# Run any other custom commands if necessary
-
-# Install necessary build tools
-
-python3.9 manage.py collectstatic --noinput
-
+python3.10 manage.py collectstatic --noinput
