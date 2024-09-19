646563bb3ecd10b92d2c4da18e096b0e0c2bda5e
Zainab Jinari <111095997+zainabJINARI@users.noreply.github.com>
2024-09-19 17:51:24 +0100

    push

--- Twichiyat/build_files.sh
+++ Twichiyat/build_files.sh
@@ -2,9 +2,9 @@
 
 
 # Ensure pip is installed and upgrade pip
-python3 -m ensurepip --upgrade
+python3.9 -m ensurepip --upgrade
 
-python3 -m pip install --upgrade pip setuptools wheel
+python3.9 -m pip install --upgrade pip setuptools wheel
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
