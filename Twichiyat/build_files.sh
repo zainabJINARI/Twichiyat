7d8ec64c1247fab0b76e1fee8b2f6f27cbf54dd6
Zainab Jinari <111095997+zainabJINARI@users.noreply.github.com>
2024-09-19 17:31:07 +0100

    push

--- Twichiyat/build_files.sh
+++ Twichiyat/build_files.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+apt-get update && apt-get install -y build-essential python3-dev
 
 # Ensure pip is installed and upgrade pip
 python3.9 -m ensurepip --upgrade
-python3.9 -m pip install --upgrade pip
 
+python3.9 -m pip install --upgrade pip setuptools wheel
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
 # Collect static files
-python3.9 manage.py collectstatic --noinput
-
+pip install --disable-pip-version-check --target . --upgrade -r requirements.txt
 # Run any other custom commands if necessary
+
+# Install necessary build tools
+
+
