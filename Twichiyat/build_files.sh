c057c96a1e1c64fc72e8d368974f134cc8318104
Zainab Jinari <111095997+zainabJINARI@users.noreply.github.com>
2024-09-19 16:20:50 +0100

    push

--- Twichiyat/build_files.sh
+++ Twichiyat/build_files.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Ensure pip is installed and upgrade pip
-python -m ensurepip --upgrade
-python -m pip install --upgrade pip
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
 
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 
 # Run any other custom commands if necessary
